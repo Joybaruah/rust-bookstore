9ee1f684288842baa5d7884c3a75455c481c5feb
Joy Baruah <joy.baruah@wireonelabs.com>
2023-04-13 20:25:15 +0530

    Added Book Model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::prelude::*;
+use db::DB;
+use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Book {
+    pub id: String,
+    pub name: String,
+    pub author: String,
+    pub pages: usize,
+    pub created_at: DateTime<Utc>,
+    pub tags: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
 }
